c91f31b56876b7a2776a4a640d0c3190c71c02b5
Sean Shoffstall <sean@sycle.net>
2021-01-29 11:37:30 -0800

    abstracted parts of the single page form code

--- resources/views/components/common/form/date-picker.blade.php
+++ resources/views/components/common/form/date-picker.blade.php
@@ -5,7 +5,7 @@
     class="relative rounded-md shadow-sm"
 >
     <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <span class="text-blue-500 sm:text-sm">
+        <span class="text-gray-500 sm:text-sm">
             <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
             </svg>
